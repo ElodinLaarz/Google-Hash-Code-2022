f4c998ed76e57ac6db18c232ff4d1ba732a0ac1b
Caleb Geiger <48812321+ElodinLaarz@users.noreply.github.com>
2022-02-21 10:32:32 -0500

    Create simulation.py

---
+++ simulation.py
@@ -0,0 +1,35 @@
+class Simulator():
+  time_remaining : int
+  objects_of_note : list
+  current_file : str
+  solution : list
+
+  def __init__(self,total_time, parsed_objects, current_file):
+    self.time_remaining = total_time
+    self.objects_of_note = parsed_objects
+    self.current_file = current_file
+
+  # Generic function for each time step
+  def choice(self):
+    # Choose what to do at some time increment
+    return
+
+  def update_solution(self):
+    # Update solution list
+    self.solution.append(self.time_remaining)
+    return
+
+  def time_step(self):
+    if self.time_remaining == 0:
+      print('Time is already up!')
+      return
+    self.time_remaining -= 1
+    # Call all the necessary functions from here
+    self.choice()
+    self.update_solution()
+
+
+  def create_output(self):
+    with open('./outputs/' + self.current_file + '.out', w) as f:
+      f.write(f'{self.time_remaining == 0}'+'\n')
+      f.write(f'{self.solution}')
