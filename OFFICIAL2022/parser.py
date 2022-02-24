4dcbecd7f42756963a7eb9be711e3b0a2003f913
menosgeze <menosgeze@gmail.com>
2022-02-24 13:33:16 -0500

    parser basic

--- OFFICIAL2022/parser.py
+++ OFFICIAL2022/parser.py
@@ -1,13 +1,30 @@
 import inspect
 import os
 
-class AuxiliaryClass:
-    def __init__(self, att1, att2):
-        self.att1 = att1
-        self.att2 = att2
+class Contributor:
+    def __init__(self, name, n_skills, skills):
+        self.name = name
+        self.n_skills = n_skills
+        self.skills = skills
     
-    def additional_function(self):
-        pass
+    def __repr__(self):
+        R = []
+        for ele in inspect.getmembers(self):      
+            if not ele[0].startswith('_') and not ele[0].endswith('_'):
+                if not inspect.ismethod(ele[1]): 
+                    R.append((ele[0], ele[1]))
+        return str(R)
+
+
+
+class Project:
+    def __init__(self, name, duration, score, best_before, n_roles, roles):
+        self.name = name
+        self.duration = duration
+        self.score = score
+        self.best_before = best_before
+        self.n_roles = n_roles
+        self.roles = roles
 
     def __repr__(self):
         R = []
@@ -23,24 +40,63 @@ class Parser:
     def __init__(self, filename):
         self.filename = filename
         self.content = open(filename, 'r').readlines()
-        
+        for i in range(len(self.content)):
+            print(i, self.content[i].strip())
+        self.parse()
+
     def parse(self):
         # line one content
         content = [int(ele) for ele in self.content[0].split()]
-        self.att_name0 = content[0]
-        self.att_var0 = content[0]
-        self.att_name1 = content[1]
-        self.att_var1 = content[1]       
+        self.n_contributors = content[0]
+        self.C = content[0]
+        self.n_projects = content[1]
+        self.P = content[1]       
+        self.contributors = []
+        self.skills = []
+        self.projects = []
 
-
-        # next A lines
+        # next C group of lines
+        cc = 0
         self.att_name = [] # or dict() or tuple()
-        for i in range(1, 1 + self.A):
-       
-        # next B lines
-        self.att_name = []
-        for i in range(1 + self.A, 1 + self.A + self.B):
+        for i in range(self.C):
+            # line = cc + 1 
+            name = self.content[cc + 1].split()[0]
+            n_skills = int(self.content[cc + 1].split()[1])
+            print(name, n_skills)
+            skills = dict()
+            for j in range(n_skills):
+                # line = cc + 2 + j
+                skill = self.content[cc + 2 + j].split()[0]
+                level = int(self.content[cc + 2 + j].split()[1])
+                skills[skill] = level
+                self.skills.append(skill)
+            
+            cc += 1 + n_skills
+            self.contributors.append(Contributor(name, n_skills, skills))
         
+        self.skills = sorted(list(set(self.skills)))
+        
+        # next 2*P lines
+        self.projects = []
+        for i in range(self.P):
+            # line = cc + 1
+            content = self.content[cc + 1].split()
+            print(content, cc + 1)
+            name = content[0]
+            duration = int(content[1])
+            score = int(content[2])
+            best_before = int(content[3])
+            n_roles = int(content[4])
+            roles = {}
+            for j in range(n_roles):
+                # line = cc + 2 + j
+                skill = self.content[cc + 2 + j].split()[0]
+                level = int(self.content[cc + 2 + j].split()[1])
+                roles[skill] = level
+            cc += 1 + n_roles
+            self.projects.append(Project(
+                name, duration, score, best_before, n_roles, roles))
+
     def print_dir(self):
         print(self.filename)
         D = {'methods': [], 'attributes': []}
