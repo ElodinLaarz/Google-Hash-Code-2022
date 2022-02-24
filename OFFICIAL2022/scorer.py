25c1dbbb21f608b069dc11a1da46e84dc12ced5a
NebulousOinkler <sharatc@uw.edu>
2022-02-24 20:15:22 +0000

    scorer done

--- OFFICIAL2022/scorer.py
+++ OFFICIAL2022/scorer.py
@@ -1,57 +1,81 @@
 from dataclasses import dataclass
-import itertools as it
-from parser_books import Parser
+from parser import Parser, Project
 
 @dataclass
-class LibrarySubmission:
-    library: int
-    books_sent: int
-    books: list
+class Submission:
+    project_name: str
+    past_submissions: list
+    people_assigned: list
+    day: int
+    days_to_complete: int
+    best_before: int
+    max_score: int
+    context: Project
 
-    def signup_time(self, A) -> int:
-        return A.libraries[self.library].T
+    def get_new_contributors(self) -> set:
+        need_to_check = set(self.people_assigned)
+        for project in self.past_submissions:
+            need_to_check -= set(project.people_assigned)
+        return need_to_check
 
-    def books_per_day(self, A) -> int:
-        return A.libraries[self.library].M
+    def is_everyone_available(self) -> bool:
+        need_to_check = set(self.people_assigned)
+        need_to_check -= self.get_new_contributors()
+        for project in self.past_submissions:
+            #print(project.day_completed, self.day)
+            if project.day_completed <= self.day:
+                #print(project.people_assigned,'BBBB')
+                need_to_check -= set(project.people_assigned)
+        if len(need_to_check) > 0:
+            #print(need_to_check)
+            return False
+        return True
+
+    def is_everyone_leveled(self) -> bool:
+        return True
     
-    def book_scores(self, A) -> list:
-        return map(lambda x: A.book_scores[x], self.books)
+    def day_start(self) -> None:
+        while not self.is_everyone_available():
+            self.day += 1
+        if not self.is_everyone_leveled():
+            raise ValueError(f'Contributors in project {self.project_name} not levelled enough')
+
+    def get_day_completed(self) -> int:
+        self.day_start()
+        self.day_completed = self.day + self.days_to_complete
+        return self.day_completed
+    
+    def penalty(self) -> int:
+        day_completed = self.get_day_completed()
+        return max(day_completed - self.best_before, 0)
+    
+    def score(self) -> int:
+        return max(self.max_score - self.penalty(), 0)
 
 class Scorer:
-    def __init__(self, input_file='', output_file=''):
+    def __init__(self, output_file, input_file):
         self.input = input_file
         self.output = output_file
         self.A = Parser(self.input)
-        self.A.parse()
-    
-    def ScoreLibrary(self, lib, days_remaining=0):
-        days_remaining = days_remaining - lib.signup_time(self.A)
-        book_scores = lib.book_scores(self.A)
-        bpd = lib.books_per_day(self.A)
-        score = 0
-        for days in range(days_remaining):
-            score += sum(it.islice(book_scores, bpd))
-        return score
     
     def score(self):
         with open(self.output,'r') as fp:
             data = [x.strip() for x in fp.readlines() if x]
-        num_libraries = int(data.pop(0))
-        submission = []
-        seen = set()
-        for i in range(0, len(data), 2):
-            library, books_sent = *map(int,data[i].split(' ')),
-            if books_sent > 0 and not library in seen:
-                seen.add(library)
-                books = list(map(int, data[i+1].split(' ')))
-               # print(sorted(books), sorted(list(set(books))))
-                if sorted(books) != sorted(list(set(books))):
-                    raise ValueError(f'Duplicate books in library {library}')
-                submission.append(LibrarySubmission(library, books_sent, books))
-        
-        dr = self.A.D
+        num_projects = data.pop(0)
+        submissions = []
         score = 0
-        for library in submission:
-            score += self.ScoreLibrary(library, days_remaining=dr)
-            dr -= library.signup_time(self.A)
+        for i in range(0, len(data), 2):
+            project_name, contributors = data[i:i+2]
+            for p in self.A.projects:
+                if p.name == project_name:
+                    days_to_complete = p.duration
+                    day = 0
+                    best_before = p.best_before
+                    max_score = p.score
+                    project_context = p
+            
+            submission = Submission(project_name, submissions[::-1], contributors.split(' '), day, days_to_complete, best_before, max_score, project_context)
+            #print(submission)
+            score += submission.score()
+            submissions.append(submission)
         return score
