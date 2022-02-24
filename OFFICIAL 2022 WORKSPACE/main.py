ce8c02c94fdbc47d8eacfcd763a3b3bdadf707d4
Caleb Geiger <48812321+ElodinLaarz@users.noreply.github.com>
2022-02-24 12:04:52 -0500

    indent main

--- OFFICIAL 2022 WORKSPACE/main.py
+++ OFFICIAL 2022 WORKSPACE/main.py
@@ -16,28 +16,27 @@ if __name__ == '__main__':
 
         current_simulation = Simulator(p.data(), prefix)
 
-    while (current_simulation.time_remaining > 0):
-        current_simulation.time_step()
+        while (current_simulation.time_remaining > 0):
+            current_simulation.time_step()
 
-    current_simulation.create_output()
+        current_simulation.create_output()
 
-    cur_score = Score('./outputs/' + prefix + '.out')
+        cur_score = Score('./outputs/' + prefix + '.out')
 
-    total_score = 0
-    with open('./scores/' + prefix + '.score', 'r') as f:
-        prev_score = int(f.readline().strip())
+        with open('./scores/' + prefix + '.score', 'r') as f:
+            prev_score = int(f.readline().strip())
 
-        better_score = max(cur_score, prev_score)
+            better_score = max(cur_score, prev_score)
 
-        total_score += better_score
+            total_score += better_score
 
-        print(f'You scored {cur_score} on {prefix}.')
-        if(cur_score > prev_score):
-            shutil.copy('./outputs/'+prefix+'.out', './best_outputs/')
-            with open('./scores/' + prefix + '.score', 'w') as g:
-                g.write(str(better_score))
-            print('This is a better score! A new best output file has been noted.')
-        else:
-            print('This score was not an improvement.')
+            print(f'You scored {cur_score} on {prefix}.')
+            if(cur_score > prev_score):
+                shutil.copy('./outputs/'+prefix+'.out', './best_outputs/')
+                with open('./scores/' + prefix + '.score', 'w') as g:
+                    g.write(str(better_score))
+                print('This is a better score! A new best output file has been noted.')
+            else:
+                print('This score was not an improvement.')
 
     print(f'Your sum of best scores is now {total_score}')
